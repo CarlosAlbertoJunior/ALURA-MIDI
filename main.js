f8869748d478c177d668bf161e4667fd21e0c39d
CarlosAlbertoJunior <albertocloud.carlos@gmail.com>
2025-05-19 11:57:08 -0300

    Aula de Java do Alura

---
+++ main.js
@@ -0,0 +1,51 @@
+function tocaSOmPom() {
+    document.querySelector('.tecla_pom').onclick = tocaSOmPom
+
+    document.querySelector('#som_tecla_pom').play();
+
+}
+function tocaSomClap() {
+    document, querySelector('.tecla_clap').onclick = tocaSomClap
+    document.querySelector('#som_tecla_clap').play();
+
+}
+function tocaSomTim() {
+    document, querySelector('.tecla_tim').onclick = tocaSomTim
+    document.querySelector('#som_tecla_tim').play();
+
+}
+function tocaSomPuff() {
+    document, querySelector('.tecla_puff').onclick = tocaSomPuff
+    document.querySelector('#som_tecla_puff').play();
+
+}
+function tocaSomSplash() {
+    document, querySelector('.tecla_splash').onclick = tocaSomSplash
+    document.querySelector('#som_tecla_splash').play();
+
+}
+function tocaSomToim() {
+    document, querySelector('.tecla_toim').onclick = tocaSomToim
+    document.querySelector('#som_tecla_toim').play();
+
+}
+function tocaSomPsh() {
+    document, querySelector('.tecla_psh').onclick = tocaSomPsh
+    document.querySelector('#som_tecla_psh').play();
+
+}
+function tocaSomTic() {
+    document, querySelector('.tecla_tic').onclick = tocaSomTic
+    document.querySelector('#som_tecla_tic').play();
+
+}
+function tocaSomTom() {
+    document, querySelector('.tecla_tom').onclick = tocaSomTom
+    document.querySelector('#som_tecla_tom').play();
+
+}
+
+
+
+
+
